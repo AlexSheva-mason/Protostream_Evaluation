1d1d52a1fa5aaf3de5e97ac12cbebdbe1297c9ce
Alexei <alexei@smi-global.net>
2022-01-31 15:51:33 +0000

    Adds edit texts to login screen

--- app/src/main/java/alexei/sevcisen/android/protostreamevaluation/MainActivity.kt
+++ app/src/main/java/alexei/sevcisen/android/protostreamevaluation/MainActivity.kt
@@ -4,9 +4,24 @@ import alexei.sevcisen.android.protostreamevaluation.ui.theme.ProtostreamEvaluat
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -18,7 +33,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ProtostreamEvaluationTheme {
                 val navController = rememberNavController()
-                NavHost(navController = navController, startDestination = "profile") {
+                NavHost(navController = navController, startDestination = "login_screen") {
                     composable("login_screen") { LoginScreen(navController = navController) }
                     composable("main_list_screen") { MainListScreen(navController = navController) }
                 }
@@ -27,9 +42,44 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun LoginScreen(navController: NavController?) {
-
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    val localFocusManager = LocalFocusManager.current
+    val keyboardController = LocalSoftwareKeyboardController.current
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        OutlinedTextField(
+            value = email,
+            modifier = Modifier.padding(10.dp),
+            label = { Text("e-mail") },
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+            keyboardActions = KeyboardActions(
+                onNext = { localFocusManager.moveFocus(FocusDirection.Down) }
+            ),
+            onValueChange = { email = it })
+        OutlinedTextField(
+            value = password,
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier.padding(10.dp),
+            label = { Text("password") },
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+            keyboardActions = KeyboardActions(
+                onDone = { keyboardController?.hide() }
+            ),
+            onValueChange = { password = it })
+        Spacer(modifier = Modifier.height(32.dp))
+        Button(
+            modifier = Modifier.padding(10.dp),
+            onClick = { /*TODO*/ }) {
+            Text(text = "Login")
+        }
+    }
 }
 
 @Composable
